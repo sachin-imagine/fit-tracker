5dc9a516ee1b97d644f853ae9d6e66bf6d264b7e
Sachin Kumar <sachin.kumar@imagine.io>
2026-08-14 03:12:21 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -17,7 +17,7 @@
  * for now, correctness beats the small speed win.
  */
 
-const CACHE_NAME = 'fit-tracker-shell-v10';
+const CACHE_NAME = 'fit-tracker-shell-v12';
 const SHELL_FILES = [
   './',
   './index.html',
